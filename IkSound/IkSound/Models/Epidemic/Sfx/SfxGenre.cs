b5e66e9e7ffbaa4f93a4c006652b73a74780b457
ikno-the-box-guy <erwtje013@gmail.com>
2024-06-02 20:51:40 +0200

    Added SfxGenre list

---
+++ IkSound/IkSound/Models/Epidemic/Sfx/SfxGenre.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace IkSound.Models.Epidemic.Sfx;
+
+public class SfxGenre
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    [JsonPropertyName("fatherGenreId")]
+    public string FatherGenreId { get; set; }
+
+    [JsonPropertyName("tag")]
+    public string Tag { get; set; }
+
+    [JsonPropertyName("displayTag")]
+    public string DisplayTag { get; set; }
+
+    [JsonPropertyName("slug")]
+    public string Slug { get; set; }
+
+    [JsonPropertyName("numTracks")]
+    public int NumTracks { get; set; }
+}
---
+++ IkSound/IkSound/Models/Epidemic/Sfx/SfxGenreRoot.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace IkSound.Models.Epidemic.Sfx;
+
+public class SfxGenreRoot
+{
+    [JsonPropertyName("genres")]
+    public List<SfxGenre> Genres { get; set; }
+
+    [JsonPropertyName("moods")]
+    public List<object> Moods { get; set; }
+}
