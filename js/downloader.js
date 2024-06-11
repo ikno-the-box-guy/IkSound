166a05d667ed5b2b48bf26e3d0559f49be50e53b
ikno-the-box-guy <57986957+ikno-the-box-guy@users.noreply.github.com>
2024-06-11 09:52:32 +0000

    Deploying to gh-pages from  @ be6778c3a218f1cdab7d34de9d22ac6c7ecb3729 🚀

---
+++ js/downloader.js
@@ -0,0 +1,13 @@
+﻿window.download = (url, filename) => {
+    fetch(url)
+        .then(response => response.blob())
+        .then(blob => {
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = filename;
+            a.click();
+            window.URL.revokeObjectURL(url);
+            a.remove();
+        });
+}
---
+++ js/playheader.js
@@ -0,0 +1,97 @@
+﻿var interval;
+
+window.togglePlay = function (id) {
+    let audio = document.getElementById("audio-" + id);
+    let play = document.getElementById("play-" + id);
+    let pause = document.getElementById("pause-" + id);
+
+    if (audio.paused) {
+        playAudio(id, audio, play, pause);
+    } else {
+        audio.pause();
+        play.hidden = false;
+        pause.hidden = true;
+
+        clearInterval(interval);
+    }
+}
+
+window.revealButtons = function (id) {
+    let audio = document.getElementById("audio-" + id);
+    let play = document.getElementById("play-" + id);
+    let pause = document.getElementById("pause-" + id);
+
+    if (audio.paused) {
+        play.hidden = false;
+        pause.hidden = true;
+    } else {
+        play.hidden = true;
+        pause.hidden = false;
+    }
+}
+
+window.hideButtons = function (id) {
+    let play = document.getElementById("play-" + id);
+    let pause = document.getElementById("pause-" + id);
+
+    play.hidden = true;
+    pause.hidden = true;
+}
+
+window.manageWaveformClick = function(event, div, id){
+    let audio = document.getElementById("audio-" + id);
+    
+    // get mouse position relative to the waveform div
+    let rect = div.getBoundingClientRect();
+    let x = event.clientX - rect.left;
+    let width = div.offsetWidth;
+    let percentage = x / width;
+    let time = percentage * audio.duration;
+    
+    if(!Number.isNaN(time))
+        playAt(audio, id, time);
+}
+
+const playAt = function (audio, id, time){
+    let play = document.getElementById("play-" + id);
+    let pause = document.getElementById("pause-" + id);
+
+    audio.pause();
+    audio.currentTime = time;
+    
+    playAudio(id, audio, play, pause);
+}
+
+const playAudio = function (id, audio, play, pause) {
+    // Stop all other audio from playing
+    clearInterval(interval);
+    document.querySelectorAll('audio').forEach(el => {
+        if (el.id !== audio.id) {
+            el.pause()
+            el.currentTime = 0
+
+            el.previousElementSibling.style.left = '0';
+            el.previousElementSibling.style.display = 'none';
+        }
+    });
+
+    audio.play();
+    play.hidden = true;
+    pause.hidden = false;
+
+    interval = setInterval((id) => {
+        let playhead = document.getElementById("playhead-" + id);
+
+        if (audio === null || audio.ended) {
+            if(play !== null && pause !== null) {
+                play.hidden = false;
+                pause.hidden = true;
+            }
+            clearInterval(interval);
+        }
+
+        let percentage = (audio.currentTime / audio.duration) * 100;
+        playhead.style.left = percentage + "%";
+        playhead.style.display = 'block';
+    }, 20, id);
+}
