4fb719d6ecaf4ba35095a3b85131f9c2d99350c5
ikno-the-box-guy <erwtje013@gmail.com>
2024-12-04 22:33:46 +0100

    Added home page

--- iksound-vue/src/pages/HomePage.vue
+++ iksound-vue/src/pages/HomePage.vue
@@ -1,12 +1,36 @@
 ﻿<script setup>
-
-import {api} from "../api.js";
 </script>
 
 <template>
-<div>
-  
-</div>
+  <div class="px-4 text-center items-center">
+    <div class="p-">
+      <h1 class="text-8xl">Welcome to IkSound</h1>
+      <p class="text-2xl">The best place to find music and sounds for your projects</p>
+    </div>
+
+    <div>
+      <div class="m-auto mt-12 rounded-full overflow-hidden" style="width: 200px;">
+        <a href="https://github.com/ikno-the-box-guy">
+          <img class="size-full" src="/ikno.png" alt="Me"/>
+        </a>
+      </div>
+      <label class="opacity-45">(this is me btw)</label>
+    </div>
+
+    <div class="shadow-lg mt-12 p-6 w-1/2 ml-auto mr-auto text-lg">
+      <p class="mb-4">
+        This website can be used to download the free previews of the songs and sound effects from the Epidemic Sound library.
+      </p>
+      <p class="mb-4">
+        However, these preview files don't come with a license to use them in your projects. You can get the license by subscribing to Epidemic Sound.
+      </p>
+
+      <p class="mb-4">
+        If you have any questions or suggestions, feel free to contact me at <a href="mailto:iknobox@gmail.com" class="text-blue-500 cursor-pointer underline">iknobox@gmail.com</a>!
+      </p>
+    </div>
+  </div>
+
 </template>
 
 <style scoped>
