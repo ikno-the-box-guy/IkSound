496edcfcbcc330e0e2eb8164c7e31ee5b93644f3
ikno-the-box-guy <erwtje013@gmail.com>
2024-12-05 23:32:10 +0100

    Updated pages action to work with the vue application

--- iksound-vue/vite.config.js
+++ iksound-vue/vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-      vue(),
-  ],
+    plugins: [
+        vue(),
+    ],
+    base: '/IkSound/',
 })
