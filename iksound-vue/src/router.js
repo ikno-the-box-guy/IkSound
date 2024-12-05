89e2746a08bd839896afe044262a2d70bb15107d
ikno-the-box-guy <erwtje013@gmail.com>
2024-12-06 00:17:28 +0100

    Fixed router not adding webhash on deployment

--- iksound-vue/src/router.js
+++ iksound-vue/src/router.js
@@ -16,7 +16,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHashHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL + "/#/"),
     routes,
 })
 
